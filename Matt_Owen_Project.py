7ac097f6a8a37d67baab8a6e367d3172d4dcf90c
mpo5336 <mpo5336@psu.edu>
2026-04-28 12:44:35 -0400

    Actual Game Part and Play Again Feature Completed

--- Matt_Owen_Project.py
+++ Matt_Owen_Project.py
@@ -43,3 +43,39 @@ def get_valid_guess():
         except ValueError:
             print("Invalid Input. Select an Integer 1-100")
     return guess
+
+def play_game():
+    # Initial Conditions.
+    max_attempts = get_difficulty()
+    number = random.randint(1, 100)
+    attempts = 0
+    guessed_correctly = False
+    # Lets you know how many attempts you have.
+    print(f"\nGuess a Number Between 1-100. You Have {max_attempts} Attempts.\n")
+    # As long as you have attempts remaining and haven't guessed correctly, play on.
+    while attempts < max_attempts and not guessed_correctly:
+        print(f"Attempts Remaining: {max_attempts - attempts}")
+        # Checks guess to see if valid and lets you know if its high, low, correct, or no attempts remaining.
+        guess = get_valid_guess()
+        attempts += 1
+        if guess > number:
+            print("Too High, Try a Lower Number.\n")
+        elif guess < number:
+            print("Too Low, Try a Higher Number.\n")
+        else:
+            guessed_correctly = True
+            print(f"\nCongrats You Guessed the Number {number} Correctly!")
+    if not guessed_correctly:
+        print(f"\n 0 Attempts Left, the Number was {number}.")
+        
+def main():
+    # Allows you to play again if you would like to.
+    play_again = True
+    while play_again:
+        play_game()
+        answer = input("\nPlay Again? (y / n): ").strip()
+        play_again = answer in "y"
+    print("Thank You for Playing.")
+
+if __name__ == "__main__":
+    main()
