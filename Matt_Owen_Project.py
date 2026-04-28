ca3c00df42c810cc2e0adee213f20e52e5e97d3c
mpo5336 <mpo5336@psu.edu>
2026-04-28 12:42:33 -0400

    Difficulty Selection Completed

--- Matt_Owen_Project.py
+++ Matt_Owen_Project.py
@@ -7,3 +7,23 @@
 ##########################################
 
 import random
+def get_difficulty():
+    # Lets user know options for difficulty.
+    print("1-100 Number Guessing Game")
+    print("Select Difficulty:")
+    print("1. Easy (15 attempts)")
+    print("2. Medium (10 attempts)")
+    print("3. Hard (5 attempts)")
+    # Input for choice.
+    choice = input("Enter Either 1, 2, or 3: ").strip()
+    # If user inputs something that is not a choice, it lets the user know and then to try again.
+    while choice not in ("1", "2", "3"):
+        print("Invalid Choice. Select 1, 2, or 3.")
+        choice = input("Enter Either 1, 2, or 3: ").strip()
+    # Returns number of attempts based on selection.
+    if choice == "1":
+        return 15
+    elif choice == "2":
+        return 10
+    else:
+        return 5
